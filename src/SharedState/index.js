efee41f460b22617a84a05b85a551937d228bc0d
Martin89PL <marcingladkowski@gmail.com>
2020-05-18 21:54:48 +0200

    Shared state in js

---
+++ src/SharedState/index.js
@@ -0,0 +1,6 @@
+// something which we have everything access (var, function etc.)
+const account = Object.freeze({ balance: 1000}) // shallow immutable
+
+/*
+* - use libraries like Ramda, ImmutableJs
+*/
