f21dd05eee2e23fc422b0a38b47a5bc5d7b84ad1
Martin89PL <marcingladkowski@gmail.com>
2021-02-02 06:51:26 +0100

    JSON: rules

---
+++ src/JSON/index.js
@@ -0,0 +1,18 @@
+/**
+ * JAVASCRIPT OBJECT NOTATION
+ * - It's not connected with Javascript object but it's not the same!
+ * - Light: ex. XML is heavy - a lot of bytes uses, lot of tags
+ * - Only use " " -> for keys also
+ * - No comma at the end
+ * - Can nested values
+ * - Can use strings, ints, arrays, boolean
+ * - Each programming lang. has methods to parse and read it
+ */
+
+const JSON = {
+    "name": "Marcin"
+}
+
+const JavascriptObject = {
+    name: 'Marcin'
+}
